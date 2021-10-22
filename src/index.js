c1370c31ceb798659f831fed95b7e6a38229c600
Mitch Toran <mitchellht34@gmail.com>
2021-10-22 17:15:57 -0400

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+console.log('test')
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
